fe4f98c24a05fb3e0b0a3695ac1555316df2dc9f
Nathan Merritt <gnm@the-merritts.net>
2017-04-02 14:04:47 -0400

    make harvester empty capacity completely

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -1,28 +1,36 @@
 var roleHarvester = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
-        if (creep.carry.energy < creep.carryCapacity) {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        else {
-            var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION ||
-                        structure.structureType == STRUCTURE_SPAWN ||
-                        structure.structureType == STRUCTURE_TOWER) && structure.energy < structure.energyCapacity;
-                }
-            });
-            if (target) {
-                if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                }
-            }
-        }
+  /** @param {Creep} creep **/
+  run: function(creep) {
+    if (creep.memory.harvesting && creep.carry.energy === creep.carryCapacity) {
+      creep.memory.harvesting = false;
+      creep.say('depositing');
     }
+    if (!creep.memory.harvesting && creep.carry.energy === 0) {
+      creep.memory.harvesting = true;
+      creep.say('🔄 harvest');
+    }
+
+    if (creep.memory.harvesting) {
+      var source = creep.pos.findClosestByPath(FIND_SOURCES);
+      if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+      }
+    } else {
+      var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+        filter: (structure) => {
+          var type = structure.structureType;
+          return (type == STRUCTURE_EXTENSION || type == STRUCTURE_SPAWN || type == STRUCTURE_TOWER)
+             && structure.energy < structure.energyCapacity;
+        }
+      });
+      if (target) {
+        if (creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+        }
+      }
+    }
+  }
 };
 
 module.exports = roleHarvester;
