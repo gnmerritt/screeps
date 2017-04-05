1ad2e31d583fe890847a879d1752acd8aa602eb1
Nathan Merritt <gnm@the-merritts.net>
2017-04-04 21:07:54 -0400

    don't fill the tower until we have 3 creeps

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -18,10 +18,14 @@ var roleHarvester = {
         creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
       }
     } else {
-      // prioritize an empty tower over harvesting to make new creeps
-      var target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
-        filter: (structure) => structure.structureType === STRUCTURE_TOWER && structure.energy < 200
-      });
+      var target;
+      if (creep.room.find(FIND_MY_CREEPS).length > 3) {
+        // prioritize an empty tower over harvesting to make new creeps
+       target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
+          filter: (structure) => structure.structureType === STRUCTURE_TOWER && structure.energy < 200
+        });
+      }
+      // otherwise, deposit energy into the nearest empty spawn/extension/tower
       if (!target) {
         target = creep.pos.findClosestByPath(FIND_MY_STRUCTURES, {
           filter: (structure) => {
