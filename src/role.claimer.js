27d9c7373ad99a16fc129bcc0be94ff0d3942fe6
Nathan Merritt <gnm@the-merritts.net>
2017-04-11 20:55:42 -0400

    claimer can reserve now too

--- src/role.claimer.js
+++ src/role.claimer.js
@@ -5,16 +5,8 @@ var roleClaimer = {
    * @param {Creep} creep
    */
   run: function(creep) {
-    var target = creep.memory.targetRoom;
+    var target = Memory.nextClaim;
     var room = creep.room.name;
-    if (!target) {
-      var exits = Game.map.describeExits(room);
-      for (var dir in exits) {
-        target = exits[dir];
-        creep.memory.targetRoom = target;
-        break;
-      }
-    }
 
     if (target && room != target) {
       // not in the right place yet
@@ -23,16 +15,20 @@ var roleClaimer = {
     else if (room == target) {
       // we made it! go claim the new controller
       var controller = creep.room.controller;
-      if (controller && !controller.my) {
-        creep.say('claiming');
-        if (creep.claimController(controller) == ERR_NOT_IN_RANGE) {
+      if (controller) {
+        var success = creep.claimController(controller);
+        if (success === ERR_GCL_NOT_ENOUGH) {
+          creep.say('reserving');
+          success = creep.reserveController(controller);
+        }
+        if (success === ERR_NOT_IN_RANGE) {
           creep.moveTo(controller, {visualizePathStyle: {stroke: '#ffaa00'}});
         }
       }
       else {
-        // room is claimed, move on to a new room
-        creep.say('onwards');
-        creep.memory.targetRoom = null;
+        // no controller, something is broken
+        creep.say('no controller');
+        Memory.nextClaim = null;
       }
     }
   },
@@ -53,7 +49,11 @@ var roleClaimer = {
 
   spawnExpander: function(spawn) {
     spawn.room.log('Spawning an expander creep');
-    spawn.createCreep([WORK,CARRY,CLAIM,MOVE,MOVE,MOVE], undefined, {role: 'claimer'});
+    var body = [CLAIM, MOVE];
+    if (spawn.room.energyCapacityAvailable >= 1300) {
+      body = [CLAIM, CLAIM, MOVE, MOVE];
+    }
+    spawn.createCreep(body, undefined, {role: 'claimer'});
   }
 };
 
