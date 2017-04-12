b76a8adcb162624463d97a80bac863ecfd4eccdb
Nathan Merritt <gnm@the-merritts.net>
2017-04-11 20:55:13 -0400

    make creep long range pathfinding better

--- src/role.common.js
+++ src/role.common.js
@@ -1,10 +1,48 @@
+function pathToRoom(creep, name) {
+  let from = new RoomPosition(creep.pos.x, creep.pos.y, creep.room.name);
+
+  // Use `findRoute` to calculate a high-level plan for this path,
+  // prioritizing highways and owned rooms
+  let allowedRooms = { [ from.roomName ]: true };
+  Game.map.findRoute(from.roomName, name, {
+    routeCallback(roomName) {
+      let parsed = /^[WE]([0-9]+)[NS]([0-9]+)$/.exec(roomName);
+      let isHighway = (parsed[1] % 10 === 0) || (parsed[2] % 10 === 0);
+      let isMyRoom = Game.rooms[roomName] &&
+        Game.rooms[roomName].controller &&
+        Game.rooms[roomName].controller.my;
+      if (isHighway || isMyRoom) {
+        return 1;
+      } else {
+        return 2.5;
+      }
+    }
+  }).forEach(function(info) {
+    allowedRooms[info.room] = true;
+  });
+
+  let goals = [{ pos: new RoomPosition(25, 25, name), range: 20 }];
+
+  // Invoke PathFinder, allowing access only to rooms from `findRoute`
+  return PathFinder.search(from, goals, {
+    roomCallback(roomName) {
+      if (allowedRooms[roomName] === undefined) {
+        return false;
+      }
+    }
+  });
+}
+
 var common = {
   goToRoom: function(creep, name) {
+    PathFinder.use(true);
     var room = creep.room.name;
-    var route = Game.map.findRoute(room, name);
-    if (route.length > 0) {
-      var exit = creep.pos.findClosestByPath(route[0].exit);
-      creep.moveTo(exit, {reusePath: 20, visualizePathStyle: {stroke: '#ffaa00'}});
+    var route = pathToRoom(creep, name);
+
+    if (route && route.path) {
+      creep.moveByPath(route.path);
+    } else {
+      creep.say('no path');
     }
   }
 };
